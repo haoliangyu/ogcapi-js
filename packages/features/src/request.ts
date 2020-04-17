3d2af89f3a0636729c5f670a92dac9c81b14a2c5
Haoliang Yu <haoliang.yu@outlook.com>
2020-04-17 16:40:34 -0400

    try pre-commit fix

--- packages/features/src/request.ts
+++ packages/features/src/request.ts
@@ -8,7 +8,7 @@ export default async function request(url: string, params: requestParams = {}): 
   requestUrl.searchParams.set('f', 'json');
 
   for (const key in params) {
-    requestUrl.searchParams.set(key, params[key].toString());
+    requestUrl.searchParams.set(key, encodeURIComponent(params[key].toString()));
   }
 
   const res: Response = await fetch(requestUrl.href, {
