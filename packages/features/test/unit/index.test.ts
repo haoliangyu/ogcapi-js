fbe076f252e839a56c84481c2266bd9dedba0d97
Haoliang Yu <haoliang.yu@outlook.com>
2020-06-02 17:28:41 -0400

    add tests and git hooks

--- packages/features/test/unit/index.test.ts
+++ packages/features/test/unit/index.test.ts
@@ -43,3 +43,41 @@ test('getCollection() should return a collection', async function() {
   const result = await service.getCollection('test');
   expect(result).toEqual({ id: 'test', links: [] });
 });
+
+test('getFeatures() should fetch features with parameters', async function() {
+  mockRequest(
+    'https://service.com/collections/test/items?f=json&bbox=1%252C2%252C3%252C4&limit=1',
+    {
+      type: 'FeatureCollection',
+      features: [],
+    }
+  );
+
+  const service = new Service({
+    baseUrl: 'https://service.com',
+  });
+  const result = await service.getFeatures('test', {
+    bbox: [1, 2, 3, 4],
+    limit: 1,
+  });
+  expect(result).toEqual({
+    type: 'FeatureCollection',
+    features: [],
+  });
+});
+
+test('getFeature() should fetch a feature', async function() {
+  mockRequest('https://service.com/collections/test/items/a?f=json', {
+    type: 'Feature',
+    geometry: {},
+  });
+
+  const service = new Service({
+    baseUrl: 'https://service.com',
+  });
+  const result = await service.getFeature('test', 'a');
+  expect(result).toEqual({
+    type: 'Feature',
+    geometry: {},
+  });
+});
