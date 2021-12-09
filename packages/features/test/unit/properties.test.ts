8b1651703342394cc25a4f325368ec3241532849
Sebastian Frey <sebastian.frey@outlook.com>
2021-12-09 17:54:28 +0100

    add support for properties parameter

---
+++ packages/features/test/unit/properties.test.ts
@@ -0,0 +1,26 @@
+import { isValidProperties, stringifyProperties } from '../../src/properties';
+
+test('isValidProperties() should return true for valid input', () => {
+  expect(isValidProperties('PROPERTY_A')).toBe(true);
+  expect(isValidProperties(['PROPERTY_A', 'PROPERTY_B'])).toBe(true);
+});
+
+
+test('isValidProperties() should return false for invalid input', () => {
+  expect(isValidProperties('')).toBe(false);
+  expect(isValidProperties([])).toBe(false);
+  expect(isValidProperties(1 as any)).toBe(false);
+  expect(isValidProperties({} as any)).toBe(false);
+});
+
+test('stringifyProperties() should throw an error for valid input', () => {
+  expect(() => stringifyProperties([])).toThrow('invalid properties');
+});
+
+test('isValidProperties() should return the passed string for string input', () => {
+  expect(stringifyProperties('PROPERTY_A')).toBe('PROPERTY_A');
+});
+
+test('isValidProperties() should return a comma seperated properties string for array input', () => {
+  expect(stringifyProperties(['PROPERTY_A', 'PROPERTY_B'])).toBe('PROPERTY_A,PROPERTY_B');
+});
