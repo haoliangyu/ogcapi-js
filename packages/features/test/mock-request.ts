7e1f28337f529bae117ae196aa97b74d09b791fb
Sebastian Frey <sebastian.frey@outlook.com>
2021-12-14 10:35:31 +0100

    allow passing additional options to fetchMock

--- packages/features/test/mock-request.ts
+++ packages/features/test/mock-request.ts
@@ -1,11 +1,12 @@
 import fetchMock from 'fetch-mock';
 
-export default function mockRequest(url: string, response: any): any {
+export default function mockRequest(url: string, response: any, options: any = {}): any {
   fetchMock.mock(
     {
       url,
       method: 'GET',
       overwriteRoutes: true,
+      ...options,
     },
     response
   );
