415e46efa0b4b6ee35ed7611b6027a35f3816442
Sebastian Frey <sebastian.frey@outlook.com>
2023-09-10 20:51:32 +0200

    chore(request): add support for passing headers

--- packages/shared/src/request.ts
+++ packages/shared/src/request.ts
@@ -5,19 +5,13 @@
  */
 export async function request({
   url,
+  headers = {},
   params = {},
   method = 'GET',
 }: IRequestOptions): Promise<any> {
-  const searchParams = new URLSearchParams();
-  searchParams.append('f', 'json');
-
-  for (const key in params) {
-    searchParams.append(key, params[key].toString());
-  }
-
   const isGet = method === 'GET';
-  const fetchUrl = isGet ? `${url}?${searchParams.toString()}` : url;
-  const body = !isGet ? searchParams : undefined;
+  const fetchUrl = isGet ? `${url}?${toSearchParams(params).toString()}` : url;
+  const body = !isGet ? toBody(params, headers) : undefined;
   const res: Response = await fetch(fetchUrl, {
     method,
     body,
@@ -30,12 +24,53 @@ export async function request({
   return res.json();
 }
 
+const CONTENT_TYPE_HEADER = 'content-type';
+const APPLICATION_X_WWW_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+const APPLICATION_JSON = 'application/json';
+
+function toBody(params: IRequestParams, headers: IRequestHeaders) {
+  const contentType = getContentType(headers).toLowerCase();
+
+  if (contentType.startsWith(APPLICATION_X_WWW_FORM_URLENCODED)) {
+    return toSearchParams(params);
+  } else if (contentType.startsWith(APPLICATION_JSON)) {
+    return toJSON(params);
+  }
+
+  return String(params);
+}
+
+function getContentType(headers: IRequestHeaders): string {
+  const contentType = Object.keys(headers)
+    .filter((header) => header.toLowerCase() === CONTENT_TYPE_HEADER)
+    .map(header => headers[header])[0];
+
+  return typeof contentType === 'string' ? contentType : APPLICATION_X_WWW_FORM_URLENCODED;
+}
+
+function toSearchParams(params: IRequestParams) {
+  const searchParams = new URLSearchParams();
+  searchParams.append('f', 'json');
+  for (const key in params) {
+    searchParams.append(key, params[key].toString());
+  }
+  return searchParams;
+}
+
+function toJSON(params: IRequestParams) {
+  return JSON.stringify(params);
+}
 export interface IRequestParams {
+  [key: string]: any;
+}
+
+export interface IRequestHeaders {
   [key: string]: any;
 }
 
 export interface IRequestOptions {
   url: string;
-  params?: IRequestParams
+  params?: IRequestParams;
+  headers?: IRequestHeaders;
   method?: 'GET' | 'POST';
 }
