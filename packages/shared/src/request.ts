f3d1c45bbc3570d4b955929a9f6d0c5a8e88ed4f
Sebastian Frey <sebastianfrey@users.noreply.github.com>
2023-09-17 21:28:41 +0200

    Support Requirement Class "Search" from OGC API - Features Part 5 proposal (#49)
    * chore: refactor request signature from multi args to object args
    
    * chore: adopt changes of request refactoring
    
    * chore(request): add support for passing headers
    
    * chore: pass application/json header to search features
    
    * chore: pass headers to fetch
    
    * test: add tests for json handling and passing headers

--- packages/shared/src/request.ts
+++ packages/shared/src/request.ts
@@ -3,19 +3,19 @@
  * @param url
  * @param params
  */
-export async function request(
-  url: string,
-  params: IRequestParams = {}
-): Promise<any> {
-  const searchParams = new URLSearchParams();
-  searchParams.append('f', 'json');
-
-  for (const key in params) {
-    searchParams.append(key, params[key].toString());
-  }
-
-  const res: Response = await fetch(`${url}?${searchParams.toString()}`, {
-    method: 'GET',
+export async function request({
+  url,
+  headers = {},
+  params = {},
+  method = 'GET',
+}: IRequestOptions): Promise<any> {
+  const isGet = method === 'GET';
+  const fetchUrl = isGet ? `${url}?${toSearchParams(params).toString()}` : url;
+  const body = !isGet ? toBody(params, headers) : undefined;
+  const res: Response = await fetch(fetchUrl, {
+    method,
+    body,
+    headers,
   });
 
   if (!res.ok) {
@@ -25,6 +25,53 @@ export async function request(
   return res.json();
 }
 
+const CONTENT_TYPE_HEADER = 'content-type';
+const APPLICATION_X_WWW_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+const APPLICATION_JSON = 'application/json';
+
+function toBody(params: IRequestParams, headers: IRequestHeaders) {
+  const contentType = getContentType(headers).toLowerCase();
+
+  if (contentType.startsWith(APPLICATION_X_WWW_FORM_URLENCODED)) {
+    return toSearchParams(params);
+  } else if (contentType.startsWith(APPLICATION_JSON)) {
+    return toJSON(params);
+  }
+
+  return String(params);
+}
+
+function getContentType(headers: IRequestHeaders): string {
+  const contentType = Object.keys(headers)
+    .filter((header) => header.toLowerCase() === CONTENT_TYPE_HEADER)
+    .map(header => headers[header])[0];
+
+  return typeof contentType === 'string' ? contentType : APPLICATION_X_WWW_FORM_URLENCODED;
+}
+
+function toSearchParams(params: IRequestParams) {
+  const searchParams = new URLSearchParams();
+  searchParams.append('f', 'json');
+  for (const key in params) {
+    searchParams.append(key, params[key].toString());
+  }
+  return searchParams;
+}
+
+function toJSON(params: IRequestParams) {
+  return JSON.stringify(params);
+}
 export interface IRequestParams {
   [key: string]: any;
+}
+
+export interface IRequestHeaders {
+  [key: string]: any;
+}
+
+export interface IRequestOptions {
+  url: string;
+  params?: IRequestParams;
+  headers?: IRequestHeaders;
+  method?: 'GET' | 'POST';
 }
--- packages/shared/test/unit/request.test.ts
+++ packages/shared/test/unit/request.test.ts
@@ -6,7 +6,9 @@ test('request() should send a GET request with f=json', async () => {
     success: true,
   });
 
-  const result = await request('https://www.example.com');
+  const result = await request({
+    url: 'https://www.example.com',
+  });
   expect(result.success).toBe(true);
 });
 
@@ -14,7 +16,9 @@ test('request() should throw the request error', async () => {
   mockRequest('https://www.example.com?f=json', 500);
 
   try {
-    await request('https://www.example.com');
+    await request({
+      url: 'https://www.example.com',
+    });
     expect(true).toBe(false);
   } catch (error) {
     expect((error as Error).message).toBe('Internal Server Error');
@@ -26,10 +30,13 @@ test('request() should accept additional parameters', async () => {
     success: true,
   });
 
-  const result = await request('https://www.example.com', {
-    test: true,
-    num: 1,
-    content: 'test',
+  const result = await request({
+    url: 'https://www.example.com',
+    params: {
+      test: true,
+      num: 1,
+      content: 'test',
+    },
   });
   expect(result.success).toBe(true);
 });
@@ -42,8 +49,63 @@ test('request() should accept date formatted parameter', async () => {
     }
   );
 
-  const result = await request('https://www.example.com', {
-    datetime: '2014-01-01T00:00:00.000Z/2014-01-02T00:00:00.000Z',
+  const result = await request({
+    url: 'https://www.example.com',
+    params: {
+      datetime: '2014-01-01T00:00:00.000Z/2014-01-02T00:00:00.000Z',
+    },
+  });
+  expect(result.success).toBe(true);
+});
+
+test('request() should forward headers ', async () => {
+  mockRequest(
+    'https://www.example.com',
+    {
+      success: true,
+    },
+    {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+        'accept-language': '*',
+      }
+    }
+  );
+
+  const result = await request({
+    url: 'https://www.example.com',
+    method: 'POST',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded;charset=UTF-8',
+      'accept-language': '*',
+    }
+  });
+  expect(result.success).toBe(true);
+});
+
+test('request() should send JSON when content-type header is application/json ', async () => {
+  mockRequest(
+    'https://www.example.com',
+    {
+      success: true,
+    },
+    {
+      method: 'POST',
+      body: { f: 'json' },
+      headers: {
+        'content-type': 'application/json',
+      }
+    }
+  );
+
+  const result = await request({
+    url: 'https://www.example.com',
+    method: 'POST',
+    params: { f: 'json' },
+    headers: {
+      'content-type': 'application/json',
+    }
   });
   expect(result.success).toBe(true);
 });
